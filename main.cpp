2d959e6735c46b8f6ae131f70cc860ccb51008d6
yamaha3212 <67462069+yamaha3212@users.noreply.github.com>
2022-12-27 11:40:18 +0300

    Another example added

--- main.cpp
+++ main.cpp
@@ -4,16 +4,18 @@
 
 /* example of creating independent (detached) process on Windows */
 
+#define PROCESS_NAME "Process.exe"
+
 int main()
 {
 #if defined( Q_OS_WIN )
-    LPCSTR lpApplicationName = R"(Application.exe)";                  // Name of application (or full path)
-    LPSTR lpCommandLine = NULL;                                       // Entity of commandline
-    DWORD dwCreationFlags = DETACHED_PROCESS | CREATE_NO_WINDOW;      // Flags
-    STARTUPINFO si;                                                   // Descriptor for initialization process
-    PROCESS_INFORMATION pi;                                           // Descriptor of process
+    LPCSTR lpApplicationName = PROCESS_NAME; /* Or R"(Process.exe)" */ // Name of application (or full path)
+    LPSTR lpCommandLine = NULL;                                        // Entity of commandline
+    DWORD dwCreationFlags = DETACHED_PROCESS | CREATE_NO_WINDOW;       // Flags
+    STARTUPINFO si;                                                    // Descriptor for initialization process
+    PROCESS_INFORMATION pi;                                            // Descriptor of process
 
-    ZeroMemory( &si, sizeof(si) );                                    // Explicit zeroing memory
+    ZeroMemory( &si, sizeof(si) );                                     // Explicit zeroing memory
     si.cb = sizeof(si);
 
     BOOL bSuccess = CreateProcess(lpApplicationName,
@@ -24,8 +26,8 @@ int main()
                                   dwCreationFlags,
                                   NULL,
                                   NULL,
-                                  &si,                                 // Must be a pointer (or link)
-                                  &pi);                                // Must be a pointer (or link)
+                                  &si,                                  // Must be a pointer (or link)
+                                  &pi);                                 // Must be a pointer (or link)
 
                                   /*
                                    * \We can use a link to local object (si, pi), because function only needs
@@ -33,9 +35,20 @@ int main()
                                    * \after process start, they no more needed.
                                    */
     if (bSuccess) {
-        CloseHandle(pi.hThread);                                       // Descriptors must be closed
-        CloseHandle(pi.hProcess);                                      // Descriptors must be closed
+        CloseHandle(pi.hThread);                                        // Descriptors must be closed
+        CloseHandle(pi.hProcess);                                       // Descriptors must be closed
     }
+#endif
+
+    /* OR */
+
+#if defined ( Q_OS_WIN )
+    QProcess process;                                                   // Must be created on heap, probably
+    process.setCreateProcessArgumentsModifier( []( QProcess::CreateProcessArguments* args ) noexcept
+    {
+        args->inheritHandles = false;
+    } );
+    process.start( PROCESS_NAME, QStringList() );
 #endif
 
     return 0;
